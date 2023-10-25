93cb47ec4d5b32595e489e2d21a77c472790504a
DilyanaStoyanova <dilyanazhstoyanova@gmail.com>
2023-10-25 13:56:45 +0300

    Top Menu

---
+++ react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import TopIconHome from './components/TopIconHome/TopIconHome';
+
+function App() {
+
+  return (
+    <>
+      <TopIconHome />
+      </>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/TopIconHome/TopIconHome.jsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/catcafelogo.png';
+import './TopIconHome.css';  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+export default function TopIconHome() {
+
+    return (
+        <>
+            <div id="top-icon-home">
+                <a href="/"><img src={logo} /></a>
+            </div>
+            <div id="top-icon-menu" className="text-gold">
+            <FontAwesomeIcon icon={faBars} />
+            </div>
+        </>
+    );
+}
